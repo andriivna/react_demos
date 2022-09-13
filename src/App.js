01b613c709314b3f1be13f7db25a3914055acec3
andriivna <farinaanna686@gmail.com>
2022-09-14 01:01:49 +0300

    first task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Simpsons from "./components/Simpsons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(){
+    let classNameItem = 'card';
+    return(
+        <div className="wrap">
+
+
+            <div className = 'main'>
+                <Simpsons
+                    itemName = {'Homer Jay Simpson'}
+                    pic={"https://static.wikia.nocookie.net/simpsons/images/0/0c/Homer_simpson_and_donut-1090.png/revision/latest?cb=20110911170400&path-prefix=pl"}
+                />
+            </div>
+
+            <div className = 'main'>
+                <Simpsons
+                    itemName = {'Marjorie Simpson'}
+                    pic={"https://static.wikia.nocookie.net/simpsons/images/4/4d/MargeSimpson.png/revision/latest?cb=20201222215318"}
+                />
+            </div>
+
+            <div className = 'main'>
+                <Simpsons
+                    itemName = {'Bartholomew JoJo Simpson'}
+                    pic={"https://static.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20190409004756"}
+                />
+            </div>
+
+            <div className = 'main'>
+                <Simpsons
+                    itemName = {'Lisa Marie Simpson'}
+                    pic={"https://static.wikia.nocookie.net/simpsons/images/d/d5/Lisa_Simpson_official.png/revision/latest?cb=20190409004811"}
+                />
+            </div>
+
+            <div className = 'main'>
+                <Simpsons
+                    itemName = {'Abraham Jebediah'}
+                    pic={"https://static.wikia.nocookie.net/simpsons/images/a/a9/Abraham_Simpson.png/revision/latest?cb=20201222214913"}
+                />
+            </div>
+
+
+
+
+
+        </div>
+    )
 }
 
-export default App;
+    export default App;
+
+
+
---
+++ src/components/Simpsons.js
@@ -0,0 +1,15 @@
+function Simpsons(props) {
+    let{itemName, pic} = props;
+
+    let classNameItem = 'card';
+    return(
+        <div className={classNameItem}>
+            <h2>{itemName}</h2>
+            <img src={pic}/>
+
+        </div>
+    )
+}
+
+export default Simpsons;
+
