44a0cee64a39f24c379eb9fc70378a945c9dfb61
andriivna <farinaanna686@gmail.com>
2022-10-12 14:08:43 +0300

    typeScript

--- app.ts
+++ app.ts
@@ -6,30 +6,21 @@ interface IUser{
 
 const user: IUser = {name: "Max", age: 18, gender: 'male'};
 
-interface ShowSum {
-    sum: () => number
-    showSum: () => number
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc
+    return someUser
 }
 
 
+console.log(sum(1, 2));
 
-
-
-interface incAge{
-    someUser:()=> number
-    inc:()=> number
-}
-
-class Sum implements ShowSum{
-    constructor(private a:number, private b:number) {
-    }
-
-    sum(): number{
-        return this.a+this.b
-    }
-
-    showSum(): number{
-        console.log(this.a+this.b)
-        return this.a + this.b
-    }
-}
+showSum(2, 3)
+incAge(user, 2)
